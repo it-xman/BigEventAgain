ab28731ff2910ed29c7f4091ae834068ddf8918d
itxcr <52365573@qq.com>
2019-10-19 20:31:31 +0800

    获取首页的用户头像和昵称渲染到页面完成

--- web_back/tool/config.js
+++ web_back/tool/config.js
@@ -2,3 +2,4 @@ const BASE_USER = 'http://localhost:8000/';
 
 const USER_LOGIN = BASE_USER + 'admin/login';
 const USER_LOGOUT = BASE_USER + 'admin/logout';
+const GET_USERINFO = BASE_USER + 'admin/getuser';
--- web_back/tool/user.js
+++ web_back/tool/user.js
@@ -4,13 +4,20 @@ var USER = {
       type: 'post',
       url: USER_LOGIN,
       data: options.data,
-      success: options.callback
+      success: options.callback,
     })
   },
   user_logout: function (options) {
     $.ajax({
       type: 'post',
       url: USER_LOGOUT,
+      success: options.callback,
+    })
+  },
+  get_userInfo: function (options) {
+    $.ajax({
+      type: 'get',
+      url: GET_USERINFO,
       success: options.callback,
     })
   }
